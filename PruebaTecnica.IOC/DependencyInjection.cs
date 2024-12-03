3a4a3a379751846307b0691cfccbfb35cdb59331
Edg4r168 <edg4r168@gmail.com>
2024-12-02 20:29:36 -0600

    change name

--- PruebaTecnica.IOC/DependencyInjection.cs
+++ PruebaTecnica.IOC/DependencyInjection.cs
@@ -9,7 +9,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddDependencies(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AgregarRepositorios(configuration.GetConnectionString("connection") ?? "");
+        services.AddRepositories(configuration.GetConnectionString("connection") ?? "");
         services.AddServices();
 
         return services;
